f2ee87ba97782fd9828d6bcc8205b009e2a92aa0
Catalina <catalina.cimpanu@gmail.com>
2020-08-26 15:42:40 +0200

    connected database with async and try-catch

--- config/db.js
+++ config/db.js
@@ -2,17 +2,17 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI"); // is gonna get the moongoURI from default.json; we can do that cuz we installed config
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB connected."))
-    .catch((err) => {
-      console.error(err.message);
-      process.exit(1);
     });
+    console.log("MongoDB connected.");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
