c679a5b1b38cb1eb6209f87035f0cd2e9bc92fd8
Catalina <catalina.cimpanu@gmail.com>
2020-08-26 17:44:08 +0200

    added token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { check, body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 const User = require("../models/User");
 
 // @route           POST api/users
@@ -16,7 +19,7 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -24,6 +27,46 @@ router.post(
     // res.send("passed");
 
     const { name, email, password } = req.body; // deconstructed what comes through req.body
+
+    try {
+      let user = await User.findOne({ email }); // method from mongoose, find user with the email (which we set up to be unique!)
+      if (user) {
+        return res.status(400).json({ msg: "User already exists!" });
+      }
+
+      user = new User({
+        name, // instead of name: name, since both have the same name
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: "3h",
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
