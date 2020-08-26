7c3bbdd4f9517a07a6d55de4907d0d83a175e483
Catalina <catalina.cimpanu@gmail.com>
2020-08-26 23:10:34 +0200

    added delete contact functionality in the ContactItems, updated ContactContext and ContactReducer

--- client/src/contexts/contact/ContactState.js
+++ client/src/contexts/contact/ContactState.js
@@ -51,7 +51,12 @@ const ContactState = (props) => {
   };
 
   //Delete contact
-
+  const deleteContact = (id) => {
+    dispatch({
+      type: DELETE_CONTACT,
+      payload: id,
+    });
+  };
   // Set current contact
 
   // Clear current contact
@@ -67,6 +72,7 @@ const ContactState = (props) => {
       value={{
         contacts: state.contacts,
         addContact,
+        deleteContact,
       }}
     >
       {props.children}
--- client/src/contexts/contact/contactReducer.js
+++ client/src/contexts/contact/contactReducer.js
@@ -12,8 +12,15 @@ const ContactReducer = (state, action) => {
   switch (action.type) {
     case ADD_CONTACT:
       return {
-        ...state, // i don't understand why he put the ...state here, since it only has the contacts rray... mais bon!
+        ...state,
         contacts: [...state.contacts, action.payload],
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
       };
     default:
       return state;
