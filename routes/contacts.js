0175d191cc745efea449a906f9adf3b9f601356b
Catalina <catalina.cimpanu@gmail.com>
2020-08-26 15:16:16 +0200

    Express server set up with initial routes

---
+++ routes/contacts.js
@@ -0,0 +1,33 @@
+// CRUD function for the contacts of a user
+const express = require("express");
+const router = express.Router();
+
+// @route           GET api/contacts
+// @description     Get all user's contacts
+// @access          Private
+router.get("/", (req, res) => {
+  res.send("Get all contacts");
+});
+
+// @route           POST api/contacts
+// @description     Add new contact
+// @access          Private
+router.post("/", (req, res) => {
+  res.send("Add new contact");
+});
+
+// @route           PUT api/contacts/:id
+// @description     Update contact
+// @access          Private
+router.put("/:id", (req, res) => {
+  res.send("Update contact");
+});
+
+// @route           DELETE api/contacts/:id
+// @description     Update contact
+// @access          Private
+router.delete("/:id", (req, res) => {
+  res.send("Delete contact");
+});
+
+module.exports = router;
